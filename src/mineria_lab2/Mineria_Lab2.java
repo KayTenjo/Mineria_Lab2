270bc2ac2413b193411c0278a3121ec4e7acf3aa
Rodrigo Monsalve <kayj.tune@gmail.com>
2014-05-26 20:57:02 -0400

    Lectura y limpieza de datos yey

--- src/mineria_lab2/Mineria_Lab2.java
+++ src/mineria_lab2/Mineria_Lab2.java
@@ -6,6 +6,10 @@
 
 package mineria_lab2;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 /**
  *
  * @author Kay
@@ -16,7 +20,33 @@ public class Mineria_Lab2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Argumentos: [*.data,  *.names,  tamaño de la  lista  de  las mejores reglas ,soporte mínimo.  ]
+        
+        Scanner scanner_data = new Scanner(args[0]);
+        Scanner scanner_names = new Scanner(args[1]);
+        LinkedList<LinkedList> datos = new LinkedList();
+        int contador_eliminados = 0;
+        
+        while (scanner_data.hasNext()){
+            
+            String fila_temp = scanner_data.nextLine();
+            if(!fila_temp.contains("?")){
+            String[] array_temp = fila_temp.split(",");
+            datos.add(new LinkedList(Arrays.asList(array_temp)));
+                    }
+            
+            else{
+            
+                contador_eliminados++;
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
     }
     
 }
